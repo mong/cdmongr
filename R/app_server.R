1c8de89d0a3a5a1bb546a045cc2d744ba22bd66a
kevinthon <kevin.thon@gmail.com>
2021-12-15 14:44:55 +0100

    Enkel layout, første fane.

--- R/app_server.R
+++ R/app_server.R
@@ -5,6 +5,27 @@
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
+  
+  # orto <- cdmongr::get_ortopedi_data()
+  ind <-read.csv(system.file("app/www/file162591f71.csv", package = "cdmongr"))
+  orto_ind <- c("hoftebrudd_protdislokbrudd", 
+                "hoftebrudd_stammefiks",
+                "hoftebrudd_ventetid48",
+                "leddprotese_veldok_kne",
+                "leddprotese_veldokumentert_kopp")
+  
+  
+  names(orto_ind) <- ind$title[match(paste0("cd_", orto_ind), ind$id)]
+  
+  output$valgtInd <- renderUI({
+    # ns <- session$ns
+    if (!is.null(orto_ind)) {
+      selectInput(inputId = "valgtInd_verdi", label = "Velg indikator",
+                  choices = orto_ind, multiple = FALSE)
+    }
+  })
+  
+  
   # Heartbeat every 5 seconds, to avoid app to die when user is inactive.
   output$clock <- shiny::renderText({
     shiny::invalidateLater(1)
--- R/app_ui.R
+++ R/app_ui.R
@@ -6,10 +6,40 @@
 #' @noRd
 app_ui <- function(request) {
   tagList(
-    fluidPage(
-      h1("cdmongr"),
-      p(paste0("cdmongr v", installed.packages()["cdmongr", 3])),
-      shiny::textOutput("clock")
+    # fluidPage(
+    #   h1("cdmongr"),
+    #   p(paste0("cdmongr v", installed.packages()["cdmongr", 3])),
+    #   shiny::textOutput("clock")
+    # )
+    
+    navbarPage(id = "cdmongr_app_id",
+               
+               # title = div(a(includeHTML(system.file('www/logo.svg', package='rapbase'))),
+               #             regTitle),
+               # windowTitle = regTitle,
+               # theme = "rap/bootstrap.css",
+               title = "SKDE",
+               
+               shiny::tabPanel("Leddprotese",
+                               shinyjs::useShinyjs(),
+                               shinyalert::useShinyalert(),
+                               h4("Kvalitetsindikatorer fra medisinske kvalitetsregistre innen muskel og skjelett"),
+                               fluidRow(
+                                 column(6,
+                                        uiOutput(outputId = "valgtInd"),
+                                        selectInput(inputId = "plotType", label = "Velg plott",
+                                                    choices = c("Spagettiplott", "Abacusplott",
+                                                                "Panelplot"), multiple = F)
+                                 ),
+                                 column(6,
+                                        selectInput(inputId = "velgHF", label = "Velg helseforetak",
+                                                    choices = c("shus1", "shus2",
+                                                                "shus3"), multiple = F)
+                                 )
+                               )
+               )
+               
+               
     )
   )
 }
