1d1791bbd9ba08ea1a9d64dedf620f02898c9eb4
kevinthon <kevin.thon@gmail.com>
2021-12-16 20:13:09 +0100

    delvis fungerende versjon

--- R/modul_kvalind.R
+++ R/modul_kvalind.R
@@ -28,7 +28,7 @@ kvalind_ui <- function(id) {
 kvalind_server <- function(input, output, session, ind_info, ind_navn, shus_valg) {
   ns <- session$ns
   names(ind_navn) <- ind_info$title[match(ind_navn, ind_info$id)]
-  
+
   # extract data
   all_data <- NULL
   for (i in ind_navn) {
@@ -43,30 +43,30 @@ kvalind_server <- function(input, output, session, ind_info, ind_navn, shus_valg
       all_data <- rbind(all_data, ind_data)
     }
   }
-  
+
   output$valgtInd <- renderUI({
     if (!is.null(ind_navn)) {
       selectInput(inputId = ns("valgtInd_verdi"), label = "Velg indikator",
                   choices = ind_navn, multiple = FALSE)
     }
   })
-  
+
   output$valgtShus <- renderUI({
     if (!is.null(shus_valg)) {
       selectInput(inputId = "valgtShus_verdi", label = "Velg sykehus",
                   choices = shus_valg, multiple = TRUE, selected = shus_valg)
     }
   })
-  
+
   output$plot <- plotly::renderPlotly(
-    forbered_plot(kvaldata=all_data, indikator = input$valgtInd_verdi, 
-                  shus_valg=input$valgtShus_verdi) %>% 
+    forbered_plot(kvaldata=all_data, indikator = input$valgtInd_verdi,
+                  shus_valg=if (!is.null(input$valgtShus_verdi)) {input$valgtShus_verdi} else {shus_valg}) %>%
       plotly::highlight_key(~unit_name) %>%
-      plotly::plot_ly(x=~year, y=~andel, color=~unit_name, type="scatter", mode="lines") %>%  
+      plotly::plot_ly(x=~year, y=~andel, color=~unit_name, type="scatter", mode="lines") %>%
       plotly::highlight(on = "plotly_hover")
   )
-  
-  
+
+
   output$showdata <- shiny::renderDataTable(all_data %>%
                                               dplyr::filter(.data[["ind_id"]] %in% input$valgtInd_verdi))
 }
